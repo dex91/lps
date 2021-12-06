1dd8129a60058d06127e45dd5089cecbbabe7d63
Benjamin Aranda-Mendoza <b.aranda@outlook.de>
2021-12-06 14:07:26 +0100

    added basic style of app w/ bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { StartsiteComponent } from './startsite/startsite.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
-    StartsiteComponent
+    StartsiteComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
