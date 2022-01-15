9f72d8f464eeb9e7ebe48aec41b511e1c974e3b0
Benjamin Aranda Mendoza <b.aranda@outlook.de>
2022-01-15 22:40:19 +0100

    version 1 des prüfungsmodus

--- src/app/simulate-test/simulate-test.component.ts
+++ src/app/simulate-test/simulate-test.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { examValue, Modus } from '../dateninterfaces';
+import { DatabaseMysqlService } from '../database-mysql.service';
+import { ActivatedRoute, Router, NavigationStart, Event } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'lps-simulate-test',
@@ -7,9 +12,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulateTestComponent implements OnInit {
 
-  constructor() { }
+  modus?: Modus;
+  exam?: examValue;
+  moduleModus: String = "pruefung";
+  poolSelected: Boolean = false;
+  poolURIName: String = "";
+
+  examObserver: Observable<examValue> = new Observable(observer => {
+    observer.next(this.db.getExamValue());
+    observer.complete();
+});
+
+  constructor(
+    private db: DatabaseMysqlService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.modus = this.db.getMode();
+    this.exam = this.db.getExamValue();
+    this.modus = { ...this.modus, mode: this.moduleModus, lernmodus: false ,teilpruefung: false, pruefungsmodus: true };
+    this.db.setMode(this.modus);
+
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+
+        if (event.navigationTrigger === 'popstate') {
+          this.db.exitExam();
+        }
+      }
+  });
+
+  }
 
   ngOnInit(): void {
+
+  this.route.paramMap.subscribe(nav => {
+    this.poolURIName = String(nav.get('poolURIName'));
+  });
+
+  if (this.poolURIName !== 'null') {
+    this.poolSelected = true;
+
+    if(this.exam?.warning)
+    {
+      setInterval(() => {
+        this.examObserver.subscribe(examValues => {
+          this.exam = examValues;
+        });
+      }, 150);
+
+    this.startExam();
+    }
+    else {
+      this.router.navigate(['pruefung']);
+    }
+  }
+
+  }
+
+  warningAccepted(){
+    this.exam = { warning: true}
+    this.db.setExamValue(this.exam);
+  }
+
+  startExam() {
+    //this.exam = { examStarted: true, examTimer: 300, examProgress: 50, examFailed: false, examFailedQuestion: 0, examRightQuestion: 0, warning: true }
+    this.exam = { ...this.exam , examStarted: true, examTimer: 800, examProgressPercent: 0, showProgress: true }
+    this.db.setExamValue(this.exam);
+    this.router.navigate(['pruefung', this.poolURIName, 1]);
+  }
+
+  exitExam() {
+    this.db.exitExam();
   }
 
 }
