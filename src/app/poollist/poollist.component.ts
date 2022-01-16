d3aa9c356e311dedad4257f77fd570107df2ee12
Benjamin Aranda Mendoza <b.aranda@outlook.de>
2022-01-16 13:52:45 +0100

    funktionen für prüfung hinzugefügt

--- src/app/poollist/poollist.component.ts
+++ src/app/poollist/poollist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionPool, Modus } from '../dateninterfaces';
 import { DatabaseMysqlService } from '../database-mysql.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lps-poollist',
@@ -11,15 +12,63 @@ export class PoollistComponent implements OnInit {
 
   pools?: QuestionPool[];
   modus?: Modus;
+  redirectUriName: String = '';
 
   constructor(
-    private db: DatabaseMysqlService
+    private db: DatabaseMysqlService,
+    private router: Router,
   ) {
     this.db.APIgetPools().subscribe(pools => this.pools = pools);
     this.modus = this.db.getMode();
    }
 
   ngOnInit(): void {
+  }
+
+  selectPool(poolName: String, auswahlListe: HTMLElement, selectedFrage: Number)
+  {
+    let poolAuswahl = document.getElementById(selectedFrage.toString());
+    let startButton = document.getElementById('startBtn');
+
+    if(poolAuswahl?.classList.contains('active')) {
+      poolAuswahl?.classList.remove('active');
+
+      for(let i = 0; i < auswahlListe.children.length; i++)
+      {
+        if(auswahlListe.children[i].classList.contains('disabled')){
+          auswahlListe.children[i].classList.remove('disabled');
+        }
+      }
+
+    } else {
+      poolAuswahl?.classList.add('active');
+
+      for(let i = 0; i < auswahlListe.children.length; i++)
+      {
+        if(!auswahlListe.children[i].classList.contains('active')){
+          auswahlListe.children[i].classList.add('disabled');
+        }
+      }
+
+    }
+
+    if(startButton?.hasAttribute('hidden')) {
+      startButton.removeAttribute('hidden');
+      startButton.classList.remove('btn-outline-primary');
+      startButton.classList.add('btn-outline-success');
+      startButton.classList.add('blink_me');
+    } else {
+      startButton?.setAttribute('hidden', '');
+      startButton?.classList.remove('btn-outline-success');
+      startButton?.classList.remove('blink_me');
+      startButton?.classList.add('btn-outline-primary');
+    }
+
+    return this.redirectUriName = poolName;
+  }
+
+  goForward() {
+    this.router.navigate([this.modus?.mode, this.redirectUriName]);
   }
 
 }
